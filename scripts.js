e83da65ed17c12a65c00444da34773a5c5c3efa3
William Campbell <me@williamcampbell.ie>
2020-11-03 10:59:20 +0100

    testingwithtim

--- scripts.js
+++ scripts.js
@@ -12,29 +12,23 @@ function shuffle(array) {
     }
     return array;
   }
+
 let cardsDealt = shuffle(cardArray).slice(0,10);
 let  randomTF = shuffle(TFList);
   
 let layOut = [];
 let twentyRandom = shuffle(oneToTwenty)
-// console.log(cardsDealt)
 let thisOrder = 0
 let element = {};
   
 for (let i = 0; i <10; i++) {
-    // // console.log(twentyRandom[i]);
-    // layOut.splice([twentyRandom[i]],0, cardsDealt[i][language1])
     layOut.splice([twentyRandom[i]],0, { "display" : cardsDealt[i][language1], "match": cardsDealt[i][language2]  });
-    // layOut.splice[twentyRandom[i]] = { "display" : cardsDealt[i][language1], "match": cardsDealt[i][language2]  };
 }
-for (let i = 10; i <20; i++) {
-    // console.log(twentyRandom[i]);
-    // layOut.splice[twentyRandom[i]] = { "display" : cardsDealt[i][language2], "match": cardsDealt[i][language1]  };
+
+for (let i = 0; i <10; i++) {
     layOut.splice([twentyRandom[i]],0, { "display" : cardsDealt[i][language2], "match": cardsDealt[i][language1]  });
-    //layOut.splice([twentyRandom[i]],0, cardsDealt[i][language2]) //  <<version 1
-    // layOut.splice([twentyRandom[i]],0, "{ " + "display: '" + cardsDealt[i][language2] + "', match:  '" +  cardsDealt[i][language1] + "' } , " ) // <<version 2
 }
-// layOut.splice([twentyRandom[9]],0, "{ " + "display: '" + cardsDealt[9][language2] + "', match:  '" +  cardsDealt[9][language1] + "' } " ) //  <<version 2
+
 console.log(layOut[4]);
-// console.log(layOut);
+console.log(layOut);
 

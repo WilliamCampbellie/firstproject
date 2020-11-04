f20de5aaab431c976262579334da6c18fa26df14
William Campbell <me@williamcampbell.ie>
2020-11-04 21:34:41 +0100

    wednesday

--- scripts.js
+++ scripts.js
@@ -1,36 +1,50 @@
+let goAhead = true;
+let pause = 3000
 let noteFirstCard = null;
+let noteSecondCard = null;
 const revealedCards = [];
-let score = 120;
+let score = 50;
 let firstOfPair = true;
-let card1OfPair = null;
-let card2OfPair = null;
-let language1 = 'es';
-let language2 = 'ga';
+// let card1OfPair = null;
+// let card2OfPair = null;
+var language1 = 'de';
+var language2 = 'en';
+
 const cards = document.querySelectorAll('.card');
-justClickedCard = ""
-cards.forEach(card => {card.addEventListener('click', playATurn); });
+let justClickedCard = ""
+function startButton(){
+var  language1 = document.getElementsByName("Language1")[0].value;
+var  language2 = document.getElementsByName("Language2")[0].value;
+document.getElementById("clickMe").value="Restart Game";
+  console.log(language1)
+  console.log(language2)
+  cards.forEach(card => card.addEventListener('click', playATurn));
+};
+deck = startGame(); // sort this button out
 
 function reply_click(clicked_id) {
-    return(clicked_id);
-    console.log(clicked_id);
-}
-
-function wait(milliseconds) {
-  const date = Date.now();
-  let currentDate = null;
-  do {
-    currentDate = Date.now();
-  } while (currentDate - date < milliseconds);
+  return(clicked_id);
 }
 
 function tester() {
   console.log("Test successful")
 }
 
-deck = startGame();
-// console.log(deck);
+function flipBack(){
+  if (score <=1) {
+    document.getElementById("score").innerHTML = "You Lose!";    
+    goAhead = false;
+  } else {
+    noteFirstCard.classList.toggle('flip');
+    noteSecondCard.classList.toggle('flip');
+    goAhead = true;
+  }
+}
+
 
 function startGame() {
+  document.getElementById("score").innerHTML = "Score " + score ;
+    
   let cardsDealt = shuffle(cardArray).slice(0,10);
   let layOut = [];
   let twentyRandom = shuffle(oneToTwenty)
@@ -43,8 +57,9 @@ function startGame() {
   
   for (let i = 0; i <10; i++) {
     layOut.splice([twentyRandom[i]],0, { "display" : cardsDealt[i][language2], "match": cardsDealt[i][language1]  });
+  console.log("later ", language1)
   }
-return layOut;
+  return layOut;
 };
 
 function dealCards() {
@@ -54,71 +69,59 @@ function dealCards() {
   }
 }
 
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-    while (0 !== currentIndex) {
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  }
-
-function playATurn() {
-  if (revealedCards.includes(Number(justClickedCard)) == false){
-      this.classList.toggle('flip');
-  if (firstOfPair == true) {
-    firstCard()
-    noteFirstCard = this
-    console.log("first card done")
-  } else {
-    secondCard();
-    console.log("doing second card")
-    this.classList.toggle('flip');
-    console.log(firstOfPair)
-    // wait(3000);
-    // noteFirstCard.classList.toggle('flip');
-    console.log("second card done")
-    
-  } 
-}
-console.log(firstOfPair)
-}
-
-function flipCard() {
-  // id="0".classList.toggle('flip');
-}
-
-dealCards();
-
-function firstCard() {
-  score = score - 1;
-  card1OfPair = justClickedCard
-  firstOfPair = false;
-  
-}
-
-function secondCard() {
-  console.log("doing second card function")
-  card2OfPair = justClickedCard
-if (deck[card1OfPair].display === deck[card2OfPair].match) {
-  revealedCards.push(Number(card1OfPair));
-  revealedCards.push(Number(card2OfPair));
-// console.log(revealedCards)
-// console.log(deck[card1OfPair])
-} else {
-  // console.log("no match")
-  // console.log(deck[card1OfPair])
-  // this.classList.toggle('flip');
-}
-firstOfPair = true;
-
-}
-
-
 function reply_click(clicked_id) {
   justClickedCard = clicked_id;
 }
 
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+function playATurn() {
+  if ((revealedCards.includes(Number(justClickedCard)) === false) && (goAhead)){
+    if (firstOfPair == true) {
+        noteFirstCard = this;
+        firstCard();
+    } else {
+        noteSecondCard = this;
+        secondCard();
+          }
+}
+}
+
+dealCards();
+function firstCard() {
+  noteFirstCard.classList.toggle('flip');
+  card1OfPair = justClickedCard
+  firstOfPair = false;
+}
+
+function secondCard() {
+  card2OfPair = justClickedCard
+  noteSecondCard.classList.toggle('flip');
+  if (deck[card1OfPair].display === deck[card2OfPair].match) {
+    tester()
+    console.log(noteSecondCard)
+    revealedCards.push(Number(card1OfPair));
+    revealedCards.push(Number(card2OfPair));
+    noteFirstCard.classList.remove('cardfront');
+    noteFirstCard.querySelector('div').classList.add('revealed')
+    noteSecondCard.querySelector('div').classList.add('revealed')
+    noteFirstCard.classList.remove('cardfront');
+  } else {
+    score = score-2;
+    document.getElementById("score").innerHTML = "Score " + score ;    
+    goAhead = false;
+    let pauseID = setTimeout(flipBack, pause);
+  }
+  firstOfPair = true;
+
+}
